853540bfe1676046521b1fbd403d0f7712aa0e05
Jeffrey Deng <jeffreyjiawendeng@gmail.com>
2025-07-28 14:16:04 -0400

    Create Set Up
    Created the Set Up files and directories.

---
+++ data/data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import random
+
+# Create seats dataframe (Assigned to Nathan)
+# Seat_ID, Table_ID, Room_ID, Noise, Brightness, Seat_Available, Table_Available, Room_Available
+
+# Create students dataframe (Assigned to Sammam)
+# Student_ID, Group_ID, Noise, Brightness, Flexibility
